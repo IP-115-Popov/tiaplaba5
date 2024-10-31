1ab5eff6d703f1c61afa49127d1efd0849effe84
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-10-31 13:58:02 +0700

    Add State.kt

---
+++ domain/src/main/java/ru/sergey/domain/State.kt
@@ -0,0 +1,36 @@
+package ru.sergey.domain
+
+class State {
+    var isFinalState = false
+    var name : String
+    var path : Map<String, String> // symvol , nameNextState
+    constructor(string: String) {
+        val arr = string.split(')')
+        this.name = arr[0]
+
+        //финнальное состояние либо должно не иметь переходов либо имя должно быть f
+        if (name == "f") isFinalState = true
+
+        if (arr.getOrNull(1) == "")
+        {
+            path = mapOf()
+            isFinalState == true
+            return
+        }
+
+        val pList = mutableMapOf<String, String>()
+
+        val arrPath = arr[1].split("|")
+        arrPath.forEach {
+            val p = it.split("->")
+            pList.put(p[0], p[1])
+        }
+
+        this.path = pList
+    }
+    constructor(name : String, path : Map<String, String>)
+    {
+        this.name = name
+        this.path = path
+    }
+}
