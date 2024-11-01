0fe31845379c6ecfdc05c6ec65ba239478710538
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-11-02 00:33:14 +0700

    Возможность добавления нескольких путей к одному состоянию

--- app/src/main/java/ru/sergey/tiap/presentation/screens/DKAScreen.kt
+++ app/src/main/java/ru/sergey/tiap/presentation/screens/DKAScreen.kt
@@ -1,9 +1,11 @@
 package ru.sergey.tiap.presentation.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -24,14 +26,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -53,43 +58,42 @@ fun DKAScreen(vm : DKAScreenViewModel = viewModel()) {
     val items = vm.DKD.collectAsState()
 
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn {
+    Column(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(Modifier.fillMaxSize(0.9f)) {
             itemsIndexed(items.value) { index, state->
                 StateItem(index , state ,vm)
             }
         }
-        Button(
-            onClick = {
-                vm.addState()
-            },
-            Modifier
-                .size(150.dp, height = 120.dp)
-                .padding(top = 50.dp)
-                .align(Alignment.BottomEnd),
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color(0xff004D40),       // цвет текста
-                containerColor = Color(0xff9ed6df)
-            ),   // цвет фона
-            border = BorderStroke(3.dp, Color.DarkGray)
-        ) {
-            Text("Add State", fontSize = 25.sp)
-        }
-        Button(
-            onClick = {
-                vm.setDKA()
-            },
-            Modifier
-                .size(150.dp, height = 120.dp)
-                .padding(top = 50.dp)
-                .align(Alignment.BottomStart),
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color(0xff004D40),       // цвет текста
-                containerColor = Color(0xff9ed6df)
-            ),   // цвет фона
-            border = BorderStroke(3.dp, Color.DarkGray)
-        ) {
-            Text(stringResource(R.string.set_dka), fontSize = 25.sp)
+        Row(Modifier.fillMaxSize()) {
+            Button(
+                onClick = {
+                    vm.addState()
+                },
+                Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(0.5f),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color(0xff004D40),       // цвет текста
+                    containerColor = Color(0xff9ed6df)
+                ),   // цвет фона
+                border = BorderStroke(3.dp, Color.DarkGray)
+            ) {
+                Text("Add State", fontSize = 25.sp)
+            }
+            Button(
+                onClick = {
+                    vm.setDKA()
+                },
+                Modifier
+                    .fillMaxSize(),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = Color(0xff004D40),       // цвет текста
+                    containerColor = Color(0xff9ed6df)
+                ),   // цвет фона
+                border = BorderStroke(3.dp, Color.DarkGray)
+            ) {
+                Text(stringResource(R.string.set_dka), fontSize = 25.sp)
+            }
         }
     }
 }
@@ -100,18 +104,71 @@ fun StateItem(index : Int, state: State , vm : DKAScreenViewModel) {
     val nextStateString = remember {mutableStateOf(state.name)}
 
     Row {
-        Column(modifier = Modifier.weight(1f)) {
+        Column(modifier = Modifier.weight(1.5f)) {
             Text(text = "Name")
             TextField(
                 value = stateString.value,
                 textStyle = TextStyle(fontSize=25.sp),
                 onValueChange = {
                     newText -> stateString.value = newText
-                    vm.updateItem(index, stateString.value, symbol.value, nextStateString.value)
+                    //vm.updateItem(index, stateString.value, symbol.value, nextStateString.value)
+                    vm.updateItemName(index, stateString.value)
                 }
             )
         }
-        Column(modifier = Modifier.weight(1f))  {
+        val keys = remember {
+            mutableStateListOf<String>("")
+        }
+        val values = remember {
+            mutableStateListOf<String>("")
+        }
+       Column(modifier = Modifier.weight(3f)) {
+//           keys.forEachIndexed { indexPath, key ->
+//                val value = values.getOrElse(indexPath, {""})
+//                PathItem(index,indexPath , stateString, key, value, vm)
+//           }
+           keys.forEachIndexed { indexPath, key ->
+                val value = values.getOrElse(indexPath, {""})
+               PathItem(index , indexPath, stateString, keys, values, vm)
+           }
+        }
+        Button(
+            onClick = {
+                val key = ""
+                val value = ""
+                keys.add(key)
+                values.add(value)
+                vm.addPathToState(index, key, value)
+            },
+            Modifier
+                .fillMaxSize()
+                //.size(30.dp, height = 30.dp)
+                .weight(1f),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color(0xff004D40),       // цвет текста
+                containerColor = Color(0xff9ed6df)
+            ),   // цвет фона
+            border = BorderStroke(3.dp, Color.DarkGray)
+        ) {
+            Text("+", fontSize = 25.sp)
+        }
+    }
+}
+@Composable
+fun PathItem(
+    indexState: Int,
+    indexPath: Int,
+    stateString: MutableState<String>,
+    keys: SnapshotStateList<String>,
+    values: SnapshotStateList<String>,
+    vm: DKAScreenViewModel
+) {
+    // Используем remember для сохранения состояния элемента
+    val symbol = remember(keys[indexPath]) { mutableStateOf(keys[indexPath]) }
+    val nextStateString = remember(values[indexPath]) { mutableStateOf(values[indexPath]) }
+
+    Row {
+        Column(modifier = Modifier.weight(1f)) {
             Text(text = "Symbol")
             TextField(
                 value = symbol.value,
@@ -119,22 +176,58 @@ fun StateItem(index : Int, state: State , vm : DKAScreenViewModel) {
                 onValueChange = { newText ->
                     // Проверяем, является ли новый текст пустым или равно 1 символу
                     if (newText.length <= 1) {
-                        symbol.value = newText // разрешаем обновление
-                        vm.updateItem(index ,stateString.value, symbol.value, nextStateString.value)
+                        symbol.value = newText
+                        keys[indexPath] = newText // Обновляем значение в keys
+                        vm.updateItemsPath(indexState, stateString.value, keys, values)
                     }
                 }
             )
         }
-        Column(modifier = Modifier.weight(1f))  {
-            Text(text = "Next state")
+        Column(modifier = Modifier.weight(1f)) {
+            Text(text = "Next")
             TextField(
                 value = nextStateString.value,
                 textStyle = TextStyle(fontSize = 25.sp),
-                onValueChange = {
-                    newText -> nextStateString.value = newText
-                    vm.updateItem(index, stateString.value, symbol.value, nextStateString.value)
+                onValueChange = { newText ->
+                    nextStateString.value = newText
+                    values[indexPath] = newText // Обновляем значение в values
+                    vm.updateItemsPath(indexState, stateString.value, keys, values)
                 }
             )
         }
     }
 }
+//@Composable
+//fun PathItem(indexState : Int, indexPath : Int, stateString : MutableState<String>, key : String, value : String, vm : DKAScreenViewModel) {
+//    val symbol = remember {mutableStateOf(key)}
+//    val nextStateString = remember {mutableStateOf(value)}
+//    Row {
+//        Column(modifier = Modifier.weight(1f)) {
+//            Text(text = "Symbol")
+//            TextField(
+//                value = symbol.value,
+//                textStyle = TextStyle(fontSize = 25.sp),
+//                onValueChange = { newText ->
+//                    // Проверяем, является ли новый текст пустым или равно 1 символу
+//                    if (newText.length <= 1) {
+//                        symbol.value = newText // разрешаем обновление
+//                        //vm.updateItem(index, stateString.value, symbol.value, nextStateString.value)
+//                        vm.updateItemsPath(indexState, stateString.value, symbol.value, nextStateString.value)
+//                    }
+//                }
+//            )
+//        }
+//        Column(modifier = Modifier.weight(1f)) {
+//            Text(text = "Next")
+//            TextField(
+//                value = nextStateString.value,
+//                textStyle = TextStyle(fontSize = 25.sp),
+//                onValueChange = { newText ->
+//                    nextStateString.value = newText
+//                    //vm.updateItem(index, stateString.value, symbol.value, nextStateString.value)
+//                    vm.updateItemsPath(indexState, indexPath, stateString.value, symbol.value, nextStateString.value)
+//                }
+//            )
+//        }
+//    }
+//}
--- app/src/main/java/ru/sergey/tiap/viewmodel/DKAScreenViewModel.kt
+++ app/src/main/java/ru/sergey/tiap/viewmodel/DKAScreenViewModel.kt
@@ -1,5 +1,6 @@
 package ru.sergey.tiap.viewmodel
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -17,23 +18,86 @@ class DKAScreenViewModel() : ViewModel() {
         _items.value = _items.value + State("f)") // Создаем mutableStateOf при добавлении
     }
 
-    fun updateItem(index : Int, stateString : String, symbol : String, newStateString : String) {
+//    fun updateItem(index : Int, stateString : String, symbol : String, newStateString : String) {
+//        _items.value = _items.value.mapIndexed {indexInMap, it ->
+//            if (index == indexInMap) {
+//                //нашли изменёный элемент
+//                //
+//                if (symbol != "" || newStateString != "") {
+//                    val a: String = stateString + ")" + symbol + "->" + newStateString
+//                    State(a)
+//                } else {
+//                    val a: String = stateString + ")"
+//                    State(a)
+//                }
+//            } else {
+//                it
+//            }
+//        }
+//    }
+    fun updateItemName(index : Int, stateString : String) {
         _items.value = _items.value.mapIndexed {indexInMap, it ->
             if (index == indexInMap) {
                 //нашли изменёный элемент
-                //
-                if (symbol != "" || newStateString != "") {
-                    val a: String = stateString + ")" + symbol + "->" + newStateString
-                    State(a)
-                } else {
-                    val a: String = stateString + ")"
-                    State(a)
-                }
+                it.copy(name = stateString)
             } else {
                 it
             }
         }
     }
+    fun <K, V> List<K>.zipToMap(values: List<V>): Map<K, V> {
+        require(size == values.size) { "Lists must have the same size" } // Проверка на одинаковую длину
+        return zip(values).associate { (key, value) -> key to value } // Создание Map с помощью zip и associate
+    }
+    fun updateItemsPath(index : Int, stateString : String, symbols : List<String>, newStatesString : List<String>) {
+        //проверил значение приходит надо занести
+        _items.value = _items.value.mapIndexed { indexInMap, it ->
+            if (index == indexInMap) {
+                //не может быть новых элементов изменяем
+                it.copy(path = symbols.zipToMap(newStatesString))
+            } else {
+                it
+            }
+        }
+//        _items.value = _items.value.mapIndexed {indexInMap, it ->
+//            if (index == indexInMap) {
+//                //не может быть новых элементов изменяем
+//                //надо найти изменяемы путь
+//                it.path
+//                it.copy(path = path)
+//            } else {
+//                it
+//            }
+//        }
+    }
+//    fun updateItemsPath(index : Int, indexPath : Int, stateString : String, symbol : String, newStateString : String) {
+//        _items.value = _items.value.mapIndexed {indexInMap, it ->
+//            if (index == indexInMap) {
+//                //не может быть новых элементов изменяем
+//                //надо найти изменяемы путь
+//                it.path
+//                it.copy(path = path)
+//            } else {
+//                it
+//            }
+//        }
+//    }
+    fun addPathToState(index : Int , symbol : String, newStateString : String) {
+        _items.value = _items.value.mapIndexed {indexInMap, it ->
+            if (index == indexInMap) {
+                val path: Map<String, String> = it.path + mapOf(symbol to newStateString);
+                it.copy(path = path)
+            } else {
+                it
+            }
+        }
+    }
+
+
+
+
+
+
     fun updateItem(oldItem: State, newItem: State) {
         _items.value = _items.value.map {
             if (it.name == oldItem.name) newItem else it
